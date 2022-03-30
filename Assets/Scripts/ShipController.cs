ef74d5285047135884130d6dc60ae653fac58a01
Luis Garcia <luism.garciac@gmail.com>
2022-03-30 20:39:48 -0300

    Materials / Sounds
    Materials / Sounds

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -5,6 +5,7 @@ public class ShipController : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
     private Vector2 movement;
+    public AudioSource shootingSound; //Declaramos la nueva variable de fuente de sonido
 
     public float speed = 1f;
 
@@ -20,6 +21,11 @@ public class ShipController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         movement = new Vector2(moveHorizontal, 0f);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            shootingSound.Play();
+        }
     }
 
     // FixedUpdate se llama en cada fixed frame-rate frame. (50 llamadas por segundo, por defecto)
